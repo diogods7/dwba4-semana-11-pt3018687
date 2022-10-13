2d4656ba22500c213b7f024d6d9db6ee240c35b8
Diogo <105064232+diogods7@users.noreply.github.com>
2022-10-13 23:49:39 +0000

    Initial commit

---
+++ JS/sobre.js
@@ -0,0 +1,10 @@
+class Sobre extends React.Component {
+    render() {
+      return
+        <div>
+          <p> O <b>Instituto Federal de Educação, Ciência e Tecnologia de São Paulo</b> – é uma autarquia federal de ensino </p>
+        </div>;
+      
+  }
+}
+ReactDOM.render(Sobre, document.getElementById('div'));
